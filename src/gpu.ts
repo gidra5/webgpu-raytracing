656e29bf33a2d50d4d4407cd1701e3b3b7713ab1
gidra5 <hranitel.52@gmail.com>
2025-06-24 22:42:28 +0300

    refactor

---
+++ src/gpu.ts
@@ -0,0 +1,349 @@
+import { assert } from './utils';
+
+const features: Partial<Record<GPUFeatureName, boolean>> = {};
+let presentationFormat: GPUTextureFormat;
+let device: GPUDevice;
+
+export const getDevice = async (context: GPUCanvasContext) => {
+  assert(navigator.gpu, 'WebGPU not supported');
+  const adapter = await navigator.gpu.requestAdapter();
+  assert(adapter, 'No GPU adapter found');
+
+  const requiredFeatures: GPUFeatureName[] = [];
+  const canTimestamp = adapter.features.has('timestamp-query');
+
+  if (canTimestamp) {
+    features['timestamp-query'] = true;
+    requiredFeatures.push('timestamp-query');
+  }
+
+  device = await adapter.requestDevice({ requiredFeatures });
+  presentationFormat =
+    navigator.gpu.getPreferredCanvasFormat?.() ?? 'bgra8unorm';
+
+  context.configure({
+    device,
+    format: presentationFormat,
+    alphaMode: 'premultiplied',
+  });
+
+  return device;
+};
+
+type SubmitHandler = (
+  encoder: GPUCommandEncoder,
+  submit: () => void
+) => Promise<void>;
+type TimestampHandler = {
+  querySet?: GPUQuerySet;
+  canTimestamp: boolean;
+  submit: SubmitHandler;
+};
+type ResultHandler = (times: BigInt64Array<ArrayBuffer>) => void;
+export const getTimestampHandler = (
+  resultHandler: ResultHandler
+): TimestampHandler => {
+  if (!features['timestamp-query']) {
+    return {
+      canTimestamp: false,
+      async submit(_, submit) {
+        submit();
+      },
+    };
+  }
+
+  const querySet = device.createQuerySet({
+    type: 'timestamp',
+    count: 2,
+  });
+
+  // buffers with MAP_READ usage can only have COPY_DST as another usage
+  // so we need to create intermediate buffer to be able to map for read
+  const resolveBuffer = device.createBuffer({
+    size: querySet.count * 8,
+    usage: GPUBufferUsage.QUERY_RESOLVE | GPUBufferUsage.COPY_SRC,
+    label: 'Query Resolve Buffer',
+  });
+  const resultBuffer = device.createBuffer({
+    size: resolveBuffer.size,
+    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
+    label: 'Query Result Read-back Buffer',
+  });
+
+  return {
+    querySet,
+    canTimestamp: true,
+    async submit(encoder: GPUCommandEncoder, submit: () => void) {
+      encoder.resolveQuerySet(querySet, 0, querySet.count, resolveBuffer, 0);
+      if (resultBuffer.mapState !== 'unmapped') {
+        submit();
+        return;
+      }
+
+      encoder.copyBufferToBuffer(
+        resolveBuffer,
+        0,
+        resultBuffer,
+        0,
+        resultBuffer.size
+      );
+
+      submit();
+
+      await mapBuffer(resultBuffer, { mode: GPUMapMode.READ }, (buffer) => {
+        const times = new BigInt64Array(buffer);
+        resultHandler(times);
+      });
+    },
+  };
+};
+
+export const mapBuffer = async (
+  buffer: GPUBuffer,
+  options: { mode: GPUMapModeFlags; offset?: GPUSize64; size?: GPUSize64 },
+  handler: (buffer: ArrayBuffer) => void
+): Promise<void> => {
+  const { mode, offset, size } = options;
+  await buffer.mapAsync(mode, offset, size);
+  handler(buffer.getMappedRange());
+  buffer.unmap();
+};
+
+export const renderPass = (
+  encoder: GPUCommandEncoder,
+  descriptor: GPURenderPassDescriptor,
+  handler: (renderPass: GPURenderPassEncoder) => void
+) => {
+  const renderPass = encoder.beginRenderPass(descriptor);
+  handler(renderPass);
+  renderPass.end();
+};
+
+export const computePass = (
+  encoder: GPUCommandEncoder,
+  descriptor: GPUComputePassDescriptor,
+  handler: (computePass: GPUComputePassEncoder) => void
+) => {
+  const computePass = encoder.beginComputePass(descriptor);
+  handler(computePass);
+  computePass.end();
+};
+
+export const renderBundlePass = (
+  descriptor: Partial<GPURenderBundleEncoderDescriptor>,
+  handler: (renderPass: GPURenderBundleEncoder) => void
+) => {
+  const renderBundle = device.createRenderBundleEncoder({
+    colorFormats: [presentationFormat],
+    ...descriptor,
+  });
+  handler(renderBundle);
+  return renderBundle.finish();
+};
+
+const bufferBuilder = (d: GPUBufferDescriptor = { size: 0, usage: 0 }) => {
+  return {
+    size(size: number) {
+      return bufferBuilder({ ...d, size });
+    },
+    usage(usage: GPUBufferUsageFlags) {
+      return bufferBuilder({ ...d, usage });
+    },
+    mapped() {
+      return bufferBuilder({ ...d, mappedAtCreation: true });
+    },
+    label(label: string) {
+      return bufferBuilder({ ...d, label });
+    },
+    build() {
+      return device.createBuffer(d);
+    },
+  };
+};
+
+export const createStorageBuffer = (size: number, label?: string) =>
+  device.createBuffer({ size, usage: GPUBufferUsage.STORAGE, label });
+
+export const createBindGroup = (d: GPUBindGroupDescriptor) =>
+  device.createBindGroup(d);
+
+type RenderPipelineDescriptor = {
+  vertexShader: (builder: PipelineBuilder) => string;
+  fragmentShader?: (builder: PipelineBuilder) => string;
+} & Omit<GPURenderPipelineDescriptor, 'fragment' | 'vertex' | 'layout'>;
+type RenderPipelineBuilderResult = {
+  pipeline: GPURenderPipeline;
+  bindGroups: GPUBindGroup[];
+};
+type PipelineBuilder = {
+  bindGroup(): string;
+  bindVarBuffer(
+    access: GPUBufferBindingType,
+    type: string,
+    buffer: GPUBuffer
+  ): string;
+};
+export const renderPipeline = (
+  x: RenderPipelineDescriptor
+): RenderPipelineBuilderResult => {
+  const bindings: {
+    visibility: GPUShaderStage;
+    type: GPUBufferBindingType;
+    buffer: GPUBuffer;
+  }[][] = [[]];
+  const createPipelineBuilder = (
+    visibility: GPUShaderStage
+  ): PipelineBuilder => ({
+    bindGroup(): string {
+      bindings.push([]);
+      return '';
+    },
+    bindVarBuffer(
+      access: GPUBufferBindingType,
+      type: string,
+      buffer: GPUBuffer
+    ) {
+      const group = bindings.length - 1;
+      const binding = bindings[group].length;
+      bindings[group].push({ buffer, visibility, type: access });
+      const qualifier =
+        access === 'read-only-storage'
+          ? 'storage, read'
+          : access === 'storage'
+            ? 'storage'
+            : 'uniform';
+      return `@group(${group}) @binding(${binding}) var<${qualifier}> ${type};`;
+    },
+  });
+
+  // TODO: TYPESCRIPT BULLSHIT
+  const fragmentShaderModule =
+    x.fragmentShader &&
+    device.createShaderModule({
+      code: x.fragmentShader(
+        createPipelineBuilder(
+          GPUShaderStage.FRAGMENT as unknown as GPUShaderStage
+        )
+      ),
+    });
+  const vertexShaderModule = device.createShaderModule({
+    code: x.vertexShader(
+      createPipelineBuilder(GPUShaderStage.VERTEX as unknown as GPUShaderStage)
+    ),
+  });
+  const bindGroupLayouts = bindings.map((group) =>
+    device.createBindGroupLayout({
+      entries: group.map(
+        ({ visibility, type }, i): GPUBindGroupLayoutEntry => ({
+          binding: i,
+          visibility: visibility as unknown as number,
+          buffer: { type },
+        })
+      ),
+    })
+  );
+  const d: any = {
+    layout: device.createPipelineLayout({ bindGroupLayouts }),
+    vertex: { module: vertexShaderModule },
+    ...x,
+  };
+
+  if (fragmentShaderModule) {
+    d.fragment = {
+      module: fragmentShaderModule,
+      targets: [{ format: presentationFormat }],
+    };
+  }
+
+  const pipeline = device.createRenderPipeline(
+    d as GPURenderPipelineDescriptor
+  );
+  const bindGroups = bindings.map((group, i) =>
+    device.createBindGroup({
+      layout: pipeline.getBindGroupLayout(i),
+      entries: group.map(
+        ({ buffer }, i): GPUBindGroupEntry => ({
+          binding: i,
+          resource: { buffer },
+        })
+      ),
+    })
+  );
+  return { pipeline, bindGroups };
+};
+
+type ComputePipelineDescriptor = {
+  shader: (builder: PipelineBuilder) => string;
+} & Omit<GPUProgrammableStage, 'module'>;
+type ComputePipelineBuilderResult = {
+  pipeline: GPUComputePipeline;
+  bindGroups: GPUBindGroup[];
+};
+export const computePipeline = (
+  x: ComputePipelineDescriptor
+): ComputePipelineBuilderResult => {
+  const bindings: {
+    visibility: GPUShaderStage;
+    type: GPUBufferBindingType;
+    buffer: GPUBuffer;
+  }[][] = [[]];
+  const createPipelineBuilder = (
+    visibility: GPUShaderStage
+  ): PipelineBuilder => ({
+    bindGroup(): string {
+      bindings.push([]);
+      return '';
+    },
+    bindVarBuffer(
+      access: GPUBufferBindingType,
+      type: string,
+      buffer: GPUBuffer
+    ) {
+      const group = bindings.length - 1;
+      const binding = bindings[group].length;
+      bindings[group].push({ buffer, visibility, type: access });
+      const qualifier =
+        access === 'read-only-storage'
+          ? 'storage, read'
+          : access === 'storage'
+            ? 'storage, read_write'
+            : 'uniform';
+      return `@group(${group}) @binding(${binding}) var<${qualifier}> ${type};`;
+    },
+  });
+
+  // TODO: TYPESCRIPT BULLSHIT
+  const module = device.createShaderModule({
+    code: x.shader(
+      createPipelineBuilder(GPUShaderStage.COMPUTE as unknown as GPUShaderStage)
+    ),
+  });
+  const bindGroupLayouts = bindings.map((group) =>
+    device.createBindGroupLayout({
+      entries: group.map(
+        ({ visibility, type }, i): GPUBindGroupLayoutEntry => ({
+          binding: i,
+          visibility: visibility as unknown as number,
+          buffer: { type },
+        })
+      ),
+    })
+  );
+
+  const pipeline = device.createComputePipeline({
+    layout: device.createPipelineLayout({ bindGroupLayouts }),
+    compute: { module, ...x },
+  });
+  const bindGroups = bindings.map((group, i) =>
+    device.createBindGroup({
+      layout: pipeline.getBindGroupLayout(i),
+      entries: group.map(
+        ({ buffer }, i): GPUBindGroupEntry => ({
+          binding: i,
+          resource: { buffer },
+        })
+      ),
+    })
+  );
+  return { pipeline, bindGroups };
+};
--- src/render.ts
+++ src/render.ts
@@ -1,144 +1,28 @@
+import {
+  computePass,
+  computePipeline,
+  createStorageBuffer,
+  getDevice,
+  getTimestampHandler,
+  renderBundlePass,
+  renderPass,
+  renderPipeline,
+} from './gpu';
 import { setGPUTime, setJSTime, setRenderTime, store } from './store';
-import { assert } from './utils';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const features: Partial<Record<GPUFeatureName, boolean>> = {};
-let presentationFormat: GPUTextureFormat;
+let imageBuffer: GPUBuffer;
+let blitRenderBundle: GPURenderBundle;
 
 const resize = () => {
   canvas.width = canvas.clientWidth * devicePixelRatio;
   canvas.height = canvas.clientHeight * devicePixelRatio;
-};
 
-const getDevice = async () => {
-  assert(navigator.gpu, 'WebGPU not supported');
-  const adapter = await navigator.gpu.requestAdapter();
-  assert(adapter, 'No GPU adapter found');
-
-  const requiredFeatures: GPUFeatureName[] = [];
-  const canTimestamp = adapter.features.has('timestamp-query');
-
-  if (canTimestamp) {
-    features['timestamp-query'] = true;
-    requiredFeatures.push('timestamp-query');
-  }
-
-  const device = await adapter.requestDevice({ requiredFeatures });
-  presentationFormat =
-    navigator.gpu.getPreferredCanvasFormat?.() ?? 'bgra8unorm';
-
-  const context = canvas.getContext('webgpu');
-  context.configure({
-    device,
-    format: presentationFormat,
-    alphaMode: 'premultiplied',
-  });
-
-  return { device, context };
-};
-
-const mapBuffer = async (
-  buffer: GPUBuffer,
-  options: { mode: GPUMapModeFlags; offset?: GPUSize64; size?: GPUSize64 },
-  handler: (buffer: ArrayBuffer) => void
-): Promise<void> => {
-  const { mode, offset, size } = options;
-  await buffer.mapAsync(mode, offset, size);
-  handler(buffer.getMappedRange());
-  buffer.unmap();
-};
-
-type SubmitHandler = (
-  encoder: GPUCommandEncoder,
-  submit: () => void
-) => Promise<void>;
-type TimestampHandler = {
-  querySet?: GPUQuerySet;
-  canTimestamp: boolean;
-  submit: SubmitHandler;
-};
-const getTimestampHandler = (device: GPUDevice): TimestampHandler => {
-  if (!features['timestamp-query']) {
-    return {
-      canTimestamp: false,
-      async submit(_, submit) {
-        submit();
-      },
-    };
-  }
-
-  const querySet = device.createQuerySet({
-    type: 'timestamp',
-    count: 2,
-  });
-
-  // buffers with MAP_READ usace can only have COPY_DST as another usage
-  // so we need to create intermediate buffer to be able to map for read
-  const resolveBuffer = device.createBuffer({
-    size: querySet.count * 8,
-    usage: GPUBufferUsage.QUERY_RESOLVE | GPUBufferUsage.COPY_SRC,
-  });
-  const resultBuffer = device.createBuffer({
-    size: resolveBuffer.size,
-    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
-  });
-
-  return {
-    querySet,
-    canTimestamp: true,
-    async submit(encoder: GPUCommandEncoder, submit: () => void) {
-      encoder.resolveQuerySet(querySet, 0, querySet.count, resolveBuffer, 0);
-      if (resultBuffer.mapState !== 'unmapped') {
-        submit();
-        return;
-      }
-
-      encoder.copyBufferToBuffer(
-        resolveBuffer,
-        0,
-        resultBuffer,
-        0,
-        resultBuffer.size
-      );
-
-      submit();
-
-      await mapBuffer(resultBuffer, { mode: GPUMapMode.READ }, (buffer) => {
-        const times = new BigInt64Array(buffer);
-        setGPUTime(Number(times[1] - times[0]));
-      });
-    },
-  };
-};
-
-const renderPass = (
-  encoder: GPUCommandEncoder,
-  descriptor: GPURenderPassDescriptor,
-  handler: (renderPass: GPURenderPassEncoder) => void
-) => {
-  const renderPass = encoder.beginRenderPass(descriptor);
-  handler(renderPass);
-  renderPass.end();
-};
-
-const computePass = (
-  encoder: GPUCommandEncoder,
-  descriptor: GPUComputePassDescriptor,
-  handler: (computePass: GPUComputePassEncoder) => void
-) => {
-  const computePass = encoder.beginComputePass(descriptor);
-  handler(computePass);
-  computePass.end();
-};
-
-const renderBundlePass = (
-  device: GPUDevice,
-  descriptor: GPURenderBundleEncoderDescriptor,
-  handler: (renderPass: GPURenderBundleEncoder) => void
-) => {
-  const renderBundle = device.createRenderBundleEncoder(descriptor);
-  handler(renderBundle);
-  return renderBundle.finish();
+  if (imageBuffer) imageBuffer.destroy();
+  const size =
+    Float32Array.BYTES_PER_ELEMENT * 4 * canvas.width * canvas.height;
+  imageBuffer = createStorageBuffer(size, 'Raytraced Image Buffer');
+  blitRenderBundle = getBlitToScreenBundle();
 };
 
 const tonemapping = /* wgsl */ `
@@ -206,32 +90,9 @@ const tonemapping = /* wgsl */ `
   }
 `;
 
-const getBlitToScreenBundle = (device: GPUDevice, imageBuffer: GPUBuffer) => {
-  const fragmentShaderModule = device.createShaderModule({
-    code: /* wgsl */ `
-      ${tonemapping}
-
-      @group(0) @binding(0) var<storage, read> imageBuffer: array<vec3f>;
-      // @group(0) @binding(1) var<uniform> commonUniforms: CommonUniforms;
-
-      const viewport = vec2u(${canvas.width}, ${canvas.height});
-
-      @fragment
-      fn main(@location(0) uv: vec2<f32>) -> @location(0) vec4f {
-        let pos = vec2u(uv * vec2f(viewport));
-        // let idx = fma(pos.y, viewport.x, pos.x);
-        let idx = pos.y * viewport.x + pos.x; 
-        let color = imageBuffer[idx];
-        let tonemapped = color;
-        // let color = lottes(raytraceImageBuffer[idx] / f32(commonUniforms.frameCounter + 1));
-        // return aces(raytraceImageBuffer[idx] / f32(commonUniforms.frameCounter + 1));
-        // return vec4(aces(imageBuffer[idx]), 1.0);
-        return vec4(tonemapped, 1.0);
-      }
-    `,
-  });
-  const vertexShaderModule = device.createShaderModule({
-    code: /* wgsl */ `
+const getBlitToScreenBundle = () => {
+  const { pipeline, bindGroups } = renderPipeline({
+    vertexShader: () => /* wgsl */ `
       // xy pos + uv
       const FULLSCREEN_QUAD = array<vec4<f32>, 6>(
         vec4(-1, 1, 0, 0),
@@ -254,103 +115,59 @@ const getBlitToScreenBundle = (device: GPUDevice, imageBuffer: GPUBuffer) => {
         output.uv = FULLSCREEN_QUAD[VertexIndex].zw;
         return output;
       }
-
     `,
-  });
-  const bindGroupLayout = device.createBindGroupLayout({
-    entries: [
-      {
-        binding: 0,
-        visibility: GPUShaderStage.FRAGMENT,
-        buffer: { type: 'read-only-storage' },
-      },
-      // {
-      //   binding: 1,
-      //   visibility: GPUShaderStage.FRAGMENT,
-      //   buffer: { type: 'uniform' },
-      // },.
-    ],
-  });
-  const pipeline = device.createRenderPipeline({
-    layout: device.createPipelineLayout({
-      bindGroupLayouts: [bindGroupLayout],
-    }),
-    // layout: 'auto',
-    vertex: { module: vertexShaderModule },
-    fragment: {
-      module: fragmentShaderModule,
-      targets: [{ format: presentationFormat }],
-    },
+    fragmentShader: (x) => /* wgsl */ `
+      ${tonemapping}
+
+      ${x.bindVarBuffer('read-only-storage', 'imageBuffer: array<vec3f>', imageBuffer)}
+      // @group(0) @binding(1) var<uniform> commonUniforms: CommonUniforms;
+
+      const viewport = vec2u(${canvas.width}, ${canvas.height});
+
+      @fragment
+      fn main(@location(0) uv: vec2<f32>) -> @location(0) vec4f {
+        let pos = vec2u(uv * vec2f(viewport));
+        // let idx = fma(pos.y, viewport.x, pos.x);
+        let idx = pos.y * viewport.x + pos.x; 
+        let color = imageBuffer[idx];
+        let tonemapped = color;
+        // let color = lottes(raytraceImageBuffer[idx] / f32(commonUniforms.frameCounter + 1));
+        // return aces(raytraceImageBuffer[idx] / f32(commonUniforms.frameCounter + 1));
+        // return vec4(aces(imageBuffer[idx]), 1.0);
+        return vec4(tonemapped, 1.0);
+      }
+    `,
     primitive: {
       topology: 'triangle-list',
       cullMode: 'back',
     },
   });
-  const bindGroup = device.createBindGroup({
-    layout: pipeline.getBindGroupLayout(0),
-    entries: [
-      { binding: 0, resource: { buffer: imageBuffer } },
-      // {
-      //   binding: 1,
-      //   resource: {
-      //     buffer: commonUniformsBuffer,
-      //   },
-      // },
-    ],
+
+  return renderBundlePass({}, (renderPass) => {
+    renderPass.setPipeline(pipeline);
+    bindGroups.forEach((bindGroup, i) => renderPass.setBindGroup(i, bindGroup));
+    renderPass.draw(6);
   });
-
-  return renderBundlePass(
-    device,
-    { colorFormats: [presentationFormat] },
-    (renderPass) => {
-      renderPass.setPipeline(pipeline);
-      renderPass.setBindGroup(0, bindGroup);
-      renderPass.draw(6);
-    }
-  );
-};
-
-const createStorageBuffer = (
-  device: GPUDevice,
-  size: number,
-  label?: string
-) => {
-  const buffer = device.createBuffer({ size, usage: GPUBufferUsage.STORAGE });
-  buffer.label = label;
-  return buffer;
 };
 
 export const init = async () => {
+  const context = canvas.getContext('webgpu');
+  const device = await getDevice(context as GPUCanvasContext);
+
   resize();
-
-  const { device, context } = await getDevice();
-
-  const { canTimestamp, querySet, submit } = getTimestampHandler(device);
-
-  const size =
-    Float32Array.BYTES_PER_ELEMENT * 4 * canvas.width * canvas.height;
-  let imageBuffer = createStorageBuffer(device, size, 'Raytraced Image Buffer');
-
-  let blitRenderBundle = getBlitToScreenBundle(device, imageBuffer);
   window.addEventListener('resize', () => {
     resize();
-
-    imageBuffer.destroy();
-    const size =
-      Float32Array.BYTES_PER_ELEMENT * 4 * canvas.width * canvas.height;
-    imageBuffer = createStorageBuffer(device, size, 'Raytraced Image Buffer');
-    blitRenderBundle = getBlitToScreenBundle(device, imageBuffer);
   });
 
-  const debugBVHRenderBundle = renderBundlePass(
-    device,
-    { colorFormats: [presentationFormat] },
-    (renderPass) => {
-      //   renderPass.setPipeline(debugBVHPipeline);
-      //   renderPass.setBindGroup(0, debugBVHBindGroup);
-      //   renderPass.draw(2, Scene.AABBS_COUNT * 12);
-    }
-  );
+  const { canTimestamp, querySet, submit } = getTimestampHandler((times) => {
+    setGPUTime(Number(times[1] - times[0]));
+  });
+
+  const debugBVHRenderBundle = renderBundlePass({}, (renderPass) => {
+    //   renderPass.setPipeline(debugBVHPipeline);
+    //   renderPass.setBindGroup(0, debugBVHBindGroup);
+    //   renderPass.draw(2, Scene.AABBS_COUNT * 12);
+  });
 
   const rpd: GPURenderPassDescriptor = {
     colorAttachments: [
@@ -372,6 +189,33 @@ export const init = async () => {
 
   const COMPUTE_WORKGROUP_SIZE_X = 16;
   const COMPUTE_WORKGROUP_SIZE_Y = 16;
+  let _computePipeline: GPUComputePipeline, computeBindGroups: GPUBindGroup[];
+  const setComputePipeline = () => {
+    ({ pipeline: _computePipeline, bindGroups: computeBindGroups } =
+      computePipeline({
+        shader: (x) => /* wgsl */ `
+          ${x.bindVarBuffer('storage', 'imageBuffer: array<vec3f>', imageBuffer)}
+
+          const viewport = vec2u(${canvas.width}, ${canvas.height});
+
+          @compute @workgroup_size(${COMPUTE_WORKGROUP_SIZE_X}, ${COMPUTE_WORKGROUP_SIZE_Y})
+          fn main(@builtin(global_invocation_id) globalInvocationId: vec3<u32>) {
+            if (any(globalInvocationId.xy > viewport)) {
+              return;
+            }
+
+            let upos = globalInvocationId.xy;
+            let idx = upos.x + upos.y * viewport.x;
+            let pos = vec2f(upos);
+
+            imageBuffer[idx] = vec3f((pos / vec2f(viewport)), 1.0);
+          }
+        `,
+      }));
+  };
+  window.addEventListener('resize', setComputePipeline);
+  setComputePipeline();
+
   async function frame() {
     const now = performance.now();
     setRenderTime(now);
@@ -379,151 +223,12 @@ export const init = async () => {
     const encoder = device.createCommandEncoder();
     rpd.colorAttachments[0].view = context.getCurrentTexture().createView();
 
-    const raytraceBindGroup0Layout = device.createBindGroupLayout({
-      entries: [
-        {
-          binding: 0,
-          visibility: GPUShaderStage.COMPUTE,
-          buffer: { type: 'storage' },
-        },
-        // {
-        //   binding: 1,
-        //   visibility: GPUShaderStage.COMPUTE,
-        //   buffer: {
-        //     type: 'storage',
-        //   },
-        // },
-        // {
-        //   binding: 2,
-        //   visibility: GPUShaderStage.COMPUTE,
-        //   buffer: {
-        //     type: 'uniform',
-        //   },
-        // },
-        // {
-        //   binding: 3,
-        //   visibility: GPUShaderStage.COMPUTE,
-        //   buffer: {
-        //     type: 'uniform',
-        //   },
-        // },
-      ],
-    });
-
-    const raytraceBindGroup1Layout = device.createBindGroupLayout({
-      entries: [
-        // {
-        //   binding: 0,
-        //   visibility: GPUShaderStage.COMPUTE,
-        //   buffer: {
-        //     type: 'read-only-storage',
-        //   },
-        // },
-        // {
-        //   binding: 1,
-        //   visibility: GPUShaderStage.COMPUTE,
-        //   buffer: {
-        //     type: 'read-only-storage',
-        //   },
-        // },
-        // {
-        //   binding: 2,
-        //   visibility: GPUShaderStage.COMPUTE,
-        //   buffer: {
-        //     type: 'read-only-storage',
-        //   },
-        // },
-      ],
-    });
-
-    const raytraceShaderModule = device.createShaderModule({
-      code: /* wgsl */ `
-        @group(0) @binding(0) var<storage, read_write> imageBuffer: array<vec3f>;
-        
-        const viewport = vec2u(${canvas.width}, ${canvas.height});
-
-        @compute @workgroup_size(${COMPUTE_WORKGROUP_SIZE_X}, ${COMPUTE_WORKGROUP_SIZE_Y})
-        fn main(@builtin(global_invocation_id) globalInvocationId: vec3<u32>) {
-          if (any(globalInvocationId.xy > viewport)) {
-            return;
-          }
-
-          let upos = globalInvocationId.xy;
-          let idx = upos.x + upos.y * viewport.x;
-          let pos = vec2f(upos);
-
-          imageBuffer[idx] = vec3f((pos / vec2f(viewport)), 1.0);
-        }
-      `,
-    });
-    const computePipeline = device.createComputePipeline({
-      layout: device.createPipelineLayout({
-        bindGroupLayouts: [raytraceBindGroup0Layout, raytraceBindGroup1Layout],
-      }),
-      compute: {
-        module: raytraceShaderModule,
-        constants: {
-          // WORKGROUP_SIZE_X: COMPUTE_WORKGROUP_SIZE_X,
-          // WORKGROUP_SIZE_Y: COMPUTE_WORKGROUP_SIZE_Y,
-          // OBJECTS_COUNT_IN_SCENE: Scene.MODELS_COUNT,
-          // MAX_BVs_COUNT_PER_MESH: Scene.MAX_NUM_BVs_PER_MESH,
-          // MAX_FACES_COUNT_PER_MESH: Scene.MAX_NUM_FACES_PER_MESH,
-        },
-      },
-    });
-    const computeBindGroup0 = device.createBindGroup({
-      layout: computePipeline.getBindGroupLayout(0),
-      entries: [
-        { binding: 0, resource: { buffer: imageBuffer } },
-        // {
-        //   binding: 1,
-        //   resource: {
-        //     buffer: rngStateBuffer,
-        //   },
-        // },
-        // {
-        //   binding: 2,
-        //   resource: {
-        //     buffer: commonUniformsBuffer,
-        //   },
-        // },
-        // {
-        //   binding: 3,
-        //   resource: {
-        //     buffer: cameraUniformBuffer,
-        //   },
-        // },
-      ],
-    });
-    const computeBindGroup1 = device.createBindGroup({
-      layout: computePipeline.getBindGroupLayout(1),
-      entries: [
-        // {
-        //   binding: 0,
-        //   resource: {
-        //     buffer: scene.facesBuffer,
-        //   },
-        // },
-        // {
-        //   binding: 1,
-        //   resource: {
-        //     buffer: scene.aabbsBuffer,
-        //   },
-        // },
-        // {
-        //   binding: 2,
-        //   resource: {
-        //     buffer: scene.materialsBuffer,
-        //   },
-        // },
-      ],
-    });
-
     // raytrace
     computePass(encoder, {}, (computePass) => {
-      computePass.setPipeline(computePipeline);
-      computePass.setBindGroup(0, computeBindGroup0);
-      computePass.setBindGroup(1, computeBindGroup1);
+      computePass.setPipeline(_computePipeline);
+      computeBindGroups.forEach((bindGroup, i) =>
+        computePass.setBindGroup(i, bindGroup)
+      );
       computePass.dispatchWorkgroups(
         Math.ceil(canvas.width / COMPUTE_WORKGROUP_SIZE_X),
         Math.ceil(canvas.height / COMPUTE_WORKGROUP_SIZE_Y),
